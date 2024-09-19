999de7dcd8ca3afe5463a08db800c6fcdf6a1b8c
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-19 17:19:31 +0300

    feat: 1. Add a function for translating an array of bits into a string. 2. Add encryption/decryption of arbitrary length codes transmitted from an arbitrary number of stations

--- CDMA-Multiplexing.py
+++ CDMA-Multiplexing.py
@@ -10,33 +10,83 @@ def make_walsh_matrix(order: int) -> np.ndarray:
         RES = np.vstack((TOP, BOTTOM))
         return RES
 
+def binary_array_to_string(binary_array):
+    # разделяем массив на группы по 8 бит
+    byte_values = [binary_array[i:i+8] for i in range(0, len(binary_array), 8)]
+    
+    # каждый бит - символ
+    characters = [chr(int(''.join(map(str, byte)), 2)) for byte in byte_values]
+    return ''.join(characters)
 
 
 if __name__ == '__main__':
     # задаем словарь кодов станций
-    stantion_codes = {
+    station_codes = {
     }
-    stantion_words = {
+    station_words = {
 
+    }
+    decoded_words = {
+        
     }
     print("Введите через пробел название станции \nи слово, которое она передает.\nДля завершения ввода введите пустую строку.")
     while True:
         try:
-            stantion, word = map(str, input().split(' '))
+            station, word = map(str, input().split(' '))
         except ValueError:
             break
         else:
-            stantion_codes[stantion] = []
-            stantion_words[stantion] = ''.join(format(ord(char), '08b') for char in word)
+            station_codes[station] = []
+            decoded_words[station] = []
+            st_str = ''.join(format(ord(char), '08b') for char in word)
+            station_words[station] = [int(bit) for bit in st_str]
+
+    # длина самого большого слова
+    max_length = 0
+
+    # определяем самое длинное слово
+    for key, value in station_words.items():
+        current_length = len(value)
+        if current_length > max_length:
+            max_length = current_length
 
     # генерируем матрицу Уолша
-    walsh_codes_matrix = make_walsh_matrix(len(stantion_codes) - 1)
+    walsh_codes_matrix = make_walsh_matrix(2**(len(station_codes) - 1) - 1)
+    print(walsh_codes_matrix)
+
+    # указываем длину кодов станций
+    size = len(walsh_codes_matrix[0])
 
     i = 0
     # каждой станции присваиваем строку из матрицы Уолша
-    for key in stantion_codes:
-        stantion_codes[key] = walsh_codes_matrix[i]
+    for key in station_codes:
+        station_codes[key] = walsh_codes_matrix[i]
         i+=1
 
-    print(stantion_codes)
-    print(stantion_words)
+    # считаем сумму сигналов    
+    signal_summ = np.zeros(size)
+
+    # чтобы все слова могли быть разной длины, берем  длину наибольшего слова
+    for i in range(max_length):
+        signal_summ.fill(0)
+
+        for key, value in station_words.items():
+            # проверка, чтоб не выйти за границы массива
+            if i < len(value):
+                if value[i] == 1:
+                    signal_summ += station_codes[key]
+                elif value[i] == 0:
+                    signal_summ -= station_codes[key]
+        
+        # расшифровка, что передала каждая из станций за такт
+        for key in decoded_words:
+            value = np.dot(signal_summ, station_codes[key]) / size
+            if value == -1:
+                decoded_words[key].append(0)
+            elif value == 1:
+                decoded_words[key].append(1)
+            else:
+                continue
+    
+    for key, value in decoded_words.items():
+        print(f'{key} says {binary_array_to_string(value)}')
