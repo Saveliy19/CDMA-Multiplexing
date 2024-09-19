ee62b02f0bda9e222de8fcc56090f3b17fa7e92e
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-19 15:34:16 +0300

    feat: Add the input of the words that the stations transmit

--- CDMA-Multiplexing.py
+++ CDMA-Multiplexing.py
@@ -16,19 +16,27 @@ if __name__ == '__main__':
     # задаем словарь кодов станций
     stantion_codes = {
     }
-    print("Введите название матрицы или пустую строку для завершения.")
+    stantion_words = {
+
+    }
+    print("Введите через пробел название станции \nи слово, которое она передает.\nДля завершения ввода введите пустую строку.")
     while True:
-        stantion = str(input())
-        if stantion == '':
+        try:
+            stantion, word = map(str, input().split(' '))
+        except ValueError:
             break
         else:
             stantion_codes[stantion] = []
+            stantion_words[stantion] = ''.join(format(ord(char), '08b') for char in word)
 
+    # генерируем матрицу Уолша
     walsh_codes_matrix = make_walsh_matrix(len(stantion_codes) - 1)
 
     i = 0
+    # каждой станции присваиваем строку из матрицы Уолша
     for key in stantion_codes:
         stantion_codes[key] = walsh_codes_matrix[i]
         i+=1
 
     print(stantion_codes)
+    print(stantion_words)
