028589da6a2029dc407730d8d4ac770f9d55149c
Saveliy19 <91277989+Saveliy19@users.noreply.github.com>
2024-09-19 15:14:46 +0300

    feat: 1. Add Walsh matrix generation. 2. add station entry and assignment of codes to them

---
+++ CDMA-Multiplexing.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def make_walsh_matrix(order: int) -> np.ndarray:
+    if order == 0:
+        return np.array([[1]])
+    else:
+        BASE = make_walsh_matrix(order - 1)
+        TOP = np.hstack((BASE, BASE))
+        BOTTOM = np.hstack((BASE, -1 * BASE))
+        RES = np.vstack((TOP, BOTTOM))
+        return RES
+
+
+
+if __name__ == '__main__':
+    # задаем словарь кодов станций
+    stantion_codes = {
+    }
+    print("Введите название матрицы или пустую строку для завершения.")
+    while True:
+        stantion = str(input())
+        if stantion == '':
+            break
+        else:
+            stantion_codes[stantion] = []
+
+    walsh_codes_matrix = make_walsh_matrix(len(stantion_codes) - 1)
+
+    i = 0
+    for key, value in enumerate(stantion_codes):
+        stantion_codes[value] = walsh_codes_matrix[i]
+        i+=1
+
+    print(stantion_codes)
